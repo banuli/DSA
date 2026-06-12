b8377901b200b214cf955d2135d3ae49d3406948
banuli <101315068+banuli@users.noreply.github.com>
2026-06-12 08:33:51 +0530

    Time: 4 ms (82.19%) | Memory: 44.5 MB (60.14%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -3,23 +3,39 @@ class Solution {
 
         if(s.length() != t.length())return false;
 
-        HashMap<Character,Integer> hm = new HashMap<>();
+        // HashMap<Character,Integer> hm = new HashMap<>();
 
-        for(int i=0;i<s.length();i++){
-            char ch = s.charAt(i);
-            hm.put(ch,hm.getOrDefault(ch,0)+1);
-        }
+        // for(int i=0;i<s.length();i++){
+        //     char ch = s.charAt(i);
+        //     hm.put(ch,hm.getOrDefault(ch,0)+1);
+        // }
         
 
-        for(int i=0;i<t.length();i++){
-            char ch = t.charAt(i);
-            hm.put(ch,hm.getOrDefault(ch,0)-1);
+        // for(int i=0;i<t.length();i++){
+        //     char ch = t.charAt(i);
+        //     hm.put(ch,hm.getOrDefault(ch,0)-1);
+        // }
+
+        // for(char key:hm.keySet()){
+        //     if(hm.get(key) != 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+        // try solutio using fixed array length
+        int[]l = new int[26];
+
+        for(int i=0;i<s.length();i++){
+            l[s.charAt(i)-'a']++;
         }
 
-        for(char key:hm.keySet()){
-            if(hm.get(key) != 0){
-                return false;
-            }
+        for(int i=0;i<t.length();i++){
+            l[t.charAt(i)-'a']--;
+        }
+
+        for(int i=0;i<l.length;i++){
+            if(l[i]!=0) return false;
         }
         return true;
     }
