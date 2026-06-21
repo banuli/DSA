3d872059b5272c885d4b67459338d07f540b0501
banuli <101315068+banuli@users.noreply.github.com>
2026-06-21 17:54:47 +0530

    Time: 8 ms (54.44%) | Memory: 46.4 MB (67.12%) - LeetSync

--- 692-top-k-frequent-words/top-k-frequent-words.java
+++ 692-top-k-frequent-words/top-k-frequent-words.java
@@ -1,24 +1,25 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-
+        
+        // Hash map freq
         HashMap<String,Integer> hm = new HashMap<>();
-
         for(int i=0;i<words.length;i++){
+          
             hm.put(words[i],hm.getOrDefault(words[i],0)+1);
         }
 
-        // put this in a priority queue sording by key of hm
-        // if key is equal use compareTo to sort lexographical
         PriorityQueue<String> pq = new PriorityQueue<>((a,b)->{
+
             if(hm.get(a) == hm.get(b)){
-                return a.compareTo(b);
+                return (a.compareTo(b));
             }else{
-                return hm.get(b)-hm.get(a);
+                return (hm.get(b)-hm.get(a));
             }
-        }) ;
-        
+        });
+
         pq.addAll(hm.keySet());
 
+        // output 
         List<String> ans = new ArrayList<>();
         int i=0;
         while(i<k){
@@ -26,5 +27,6 @@ class Solution {
             i++;
         }
         return ans;
+
     }
 }
