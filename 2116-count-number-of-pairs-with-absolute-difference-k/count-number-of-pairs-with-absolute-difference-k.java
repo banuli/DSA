f943d09de68136917da20ad1f52e47a3d0c29be3
banuli <101315068+banuli@users.noreply.github.com>
2026-07-07 19:15:53 +0530

    Time: 5 ms (81.11%) | Memory: 46 MB (21.26%) - LeetSync

--- 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
+++ 2116-count-number-of-pairs-with-absolute-difference-k/count-number-of-pairs-with-absolute-difference-k.java
@@ -1,23 +1,22 @@
 class Solution {
     public int countKDifference(int[] nums, int k) {
-        
-        // frequency hash map
+
+        // frequency map to get the nums value's frequency
         HashMap<Integer,Integer> freq = new HashMap<>();
-        int result = 0;
-
-        // for all values get the absolute difference
+        int count  = 0;
+        // iterate through the array and get the +ve and -ve difference to 
+        //see if the value exist in the freq
         for(int val:nums){
-
-            if(freq.containsKey(val + k)){
-                result += freq.get(val+k);
+            if(freq.containsKey(val+k)){
+                count += freq.get(val+k);
             }
+
             if(freq.containsKey(val-k)){
-                result += freq.get(val-k);
+                count += freq.get(val-k);
             }
 
             freq.put(val,freq.getOrDefault(val,0)+1);
         }
-
-        return result;
+        return count;
     }
 }
