2469b52717cfb04e96b342cf4a55ba3bbc72e123
banuli <101315068+banuli@users.noreply.github.com>
2026-07-12 19:36:39 +0530

    Time: 49 ms (21.73%) | Memory: 65.7 MB (80.99%) - LeetSync

--- 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
+++ 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
@@ -1,31 +1,36 @@
 class Solution {
-    public boolean isPossible(int mid,int[] price, int k){
+    public boolean isPossible(int[] price, int mid, int k){
+        int n = price.length;
+
+        // initialize the count as 1 as you will always pick
+        // the absolute smallest price as it maximizes your choice
+
         int count = 1;
-        int j =0;
-        for(int i=1;i<price.length;i++){
+        int j = 0;
+        for(int i=1;i<n;i++){
             if(price[i]-price[j] >= mid){
                 count++;
-                j = i;
+                j=i;
             }
         }
-        return count >=k;
+        return count >= k;
     }
     public int maximumTastiness(int[] price, int k) {
 
         Arrays.sort(price);
         int n = price.length;
-        
         int lo = 0;
         int hi = price[n-1];
         int ans = 0;
-        while(lo<=hi){
-            int mid = lo + (hi-lo)/2;
+        
+        while(lo <= hi){
+            int mid = (lo+hi)/2;
 
-            if(isPossible(mid,price,k)){
+            if(isPossible(price,mid,k)){
                 ans = mid;
                 lo = mid+1;
             }else{
-                hi=mid-1;
+                hi = mid-1;
             }
         }
 
