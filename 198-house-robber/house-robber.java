f9d63ba5c2d7d7fcce8db0ad0760f91abb03e21f
banuli <101315068+banuli@users.noreply.github.com>
2026-07-22 10:44:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (75.16%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,36 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
+        int n= nums.length;
 
-        // using DP trchnique
-        int n = nums.length;
+        if(n==1) return nums[0];
 
-        if(n == 1 ){
-            if(nums[0] > 0){
-                return nums[0];
-            }else{
-                return 0;
-            }
-        }
-        int[]dp = new int[n];
+        int[] dp = new int[n];
 
-        // populate the first 2 index to build the logic
-        if(nums[0] > 0){
-            dp[0] = nums[0];
-        }
-        if(dp[0] > nums[1]){
-            dp[1] = dp[0];
+        // populate the first 2
+        dp[0] = nums[0];
+        if(nums[0] > nums[1]){
+            dp[1] = nums[0];
         }else{
             dp[1] = nums[1];
         }
 
-        for(int idx=2;idx<n;idx++){
-            int pick = nums[idx] + dp[idx-2];
-            int notpick = dp[idx-1];
+        //now use the last 2 indexes as we ieterate to get the maximum profit
+        for(int i=2;i<n;i++){
 
-            dp[idx] = Math.max(pick,notpick);
+            int pick = dp[i-2] + nums[i];
+            int notpick = dp[i-1];
+
+            dp[i] = Math.max(pick,notpick); 
         }
-
         return dp[n-1];
-        
+
     }
 }
