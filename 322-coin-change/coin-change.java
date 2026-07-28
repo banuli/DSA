6b98c008a0104591f56d232e23277fbce31237d0
banuli <101315068+banuli@users.noreply.github.com>
2026-07-28 17:08:24 +0530

    Time: 16 ms (68.15%) | Memory: 46.4 MB (61.64%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,22 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount == 0) return 0;
 
-        int [] dp = new int[amount+1];
+        // base case if amount is 0
+        if(amount ==0)return 0;
 
+        int n = coins.length;
+        int[] dp = new int[amount+1];
         Arrays.fill(dp,amount+1);
         dp[0] = 0;
-
-        // for all amounts get the minimum no of coins
-        for(int i=1;i<=amount;i++){
-            for(int coin:coins){
-                if(i-coin >= 0){
-                    dp[i] = Math.min(dp[i],1+dp[i-coin]);
+        
+        // for each amount get the minimum number of coins to use
+        for(int amt = 1;amt<=amount;amt++){
+            
+            for(int i=0;i<n;i++){
+                if(amt-coins[i] >=0){
+                    dp[amt] = Math.min(dp[amt],dp[amt-coins[i]]+1);
                 }
             }
         }
 
-        return dp[amount] == amount+1?-1 : dp[amount];  
+        return dp[amount]== amount+1? -1:dp[amount];
         
     }
 }
