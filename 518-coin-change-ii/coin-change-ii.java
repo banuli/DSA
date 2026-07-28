b6ea6cd37a55e22993e3262f7cf8a67840c2da4b
banuli <101315068+banuli@users.noreply.github.com>
2026-07-28 16:51:27 +0530

    Time: 8 ms (68.88%) | Memory: 42.9 MB (84.11%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.java
+++ 518-coin-change-ii/coin-change-ii.java
@@ -1,31 +1,16 @@
 class Solution {
-    
-    public int noOfWays(int[] coins,int idx,int amount,int[][]dp){
-        if(idx < 0 || amount < 0)return 0;
-        if(amount ==0)return 1;
-
-        // if value is already obtained
-        if(dp[idx][amount] !=-1) return dp[idx][amount];
-
-        int pick = noOfWays(coins,idx,amount-coins[idx],dp);
-        int notpick = noOfWays(coins,idx-1,amount,dp);
-
-        dp[idx][amount] = pick+notpick;
-        return pick+notpick;
-    }
     public int change(int amount, int[] coins) {
-        if(amount ==0)return 1;
         int n = coins.length;
-        int[][]dp = new int[n][amount+1];
+        int [] dp = new int[amount+1];
+        dp[0] = 1;
 
-        for(int i = 0;i<n;i++){
-            Arrays.fill(dp[i],-1);
+        for(int i= 0;i<n;i++){
+            for(int amt = 1;amt<=amount;amt++){
+                if(coins[i] <= amt){
+                    dp[amt] += dp[amt - coins[i]];
+                }
+            }
         }
-        dp[0][0] = 0;
-
-        noOfWays(coins,n-1,amount,dp);
-
-        return dp[n-1][amount];
-        
+        return dp[amount];
     }
 }
