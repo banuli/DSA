2768f7edb76dc4d6fde5c3de690f3785a8535b4c
banuli <101315068+banuli@users.noreply.github.com>
2026-08-04 23:31:39 +0530

    Time: 13 ms (92.43%) | Memory: 46.1 MB (93.21%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,25 +1,22 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
+        int n = 0;
 
-        // base case if amount is 0
-        if(amount ==0)return 0;
+        // if its one coin and the coin is greater than the amount 
+        if(n==1 && coins[0] < amount)return -1;
 
-        int n = coins.length;
-        int[] dp = new int[amount+1];
-        Arrays.fill(dp,amount+1);
-        dp[0] = 0;
-        
-        // for each amount get the minimum number of coins to use
+        int[]dp = new int[amount+1];
+
+        // for each amount get the minimum number of coins that will be used
         for(int amt = 1;amt<=amount;amt++){
-            
-            for(int i=0;i<n;i++){
-                if(amt-coins[i] >=0){
-                    dp[amt] = Math.min(dp[amt],dp[amt-coins[i]]+1);
+            int ans = Integer.MAX_VALUE;
+            for(int coin:coins){
+                if(amt - coin >=0 && dp[amt-coin] != Integer.MAX_VALUE){
+                    ans = Math.min(ans,dp[amt-coin]+1);
                 }
             }
+            dp[amt] = ans;
         }
-
-        return dp[amount]== amount+1? -1:dp[amount];
-        
+        return dp[amount]==Integer.MAX_VALUE?-1:dp[amount];
     }
 }
