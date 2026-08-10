adc1bb1dfc903ab18e164e78b6855a01cd6a4617
banuli <101315068+banuli@users.noreply.github.com>
2026-08-10 11:16:39 +0530

    Time: 23 ms (91.42%) | Memory: 43.7 MB (87.00%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,29 +1,30 @@
 class Solution {
     public boolean canPartition(int[] nums) {
 
-        // check if the total sum is odd or even
         int totalSum = 0;
         for(int num:nums){
             totalSum += num;
         }
+
+        // if the total is odd then equal subset is not possible
         if(totalSum%2 != 0) return false;
 
         int targetSum = totalSum/2;
 
-        // create a dp of targetSum+1 with all false
-        // DP[0] will be true as 0 sum is possible
-        boolean[] dp = new boolean[targetSum+1];
+        // create boolean dp for the target sum and assign 0th index true
+        boolean [] dp = new boolean[targetSum+1];
         Arrays.fill(dp,false);
         dp[0] = true;
 
-        // for all values in nums
-        // check values from num to target sum by subtracting and is that value is found
+        // 1- Outer loop over nums: Each number val is considered exactly once for updating the state array.
+        // 2 - Backward iteration over curSum (targetSum down to val): Iterating backwards ensures that when computing dp[curSum], the value dp[curSum - val] comes from the previous iteration (before val was added), preventing val from being reused to compute larger sums in the same pass.
         for(int val:nums){
             for(int curSum = targetSum; curSum >= val;curSum--){
-                dp[curSum] = dp[curSum] || dp[curSum-val];
-                if(dp[targetSum] == true) return true;
+                dp[curSum] = dp[curSum] || dp[curSum - val];
             }
+            if(dp[targetSum] == true) return true;
         }
+
         return dp[targetSum];
     }
 }
