b77f028936d828649b64f508c265cb86d584954f
banuli <101315068+banuli@users.noreply.github.com>
2026-08-11 22:57:55 +0530

    Time: 3 ms (86.07%) | Memory: 43.1 MB (86.10%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }else{
+                if(st.size() == 0) return false;
+            
+                char chPop = st.pop();
+                if(ch == ')' && chPop != '(') return false;
+                if(ch == ']'&& chPop != '[') return false;
+                if((ch == '}' && chPop != '{')) return false;
+            }
+        }
+        if(st.size() != 0) return false;
+
+        return true;
+    }
+}
