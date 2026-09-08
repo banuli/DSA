283eb1e78c75a03869c83a86f80bb331e5969876
banuli <101315068+banuli@users.noreply.github.com>
2026-09-08 17:47:25 +0530

    Time: 2 ms (89.09%) | Memory: 47.9 MB (42.59%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,19 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int n = nums.length;
 
-        // greedy approch - final position as last index
+        int n= nums.length;
         int finalPosition = n-1;
 
-        // from index less than the final position see if you can reach the final position
-        for(int i = n-2;i>=0;i--){
+        //go backward from the finalPosition -1 th index
+        // and see if you can reach the final position from ther
+        // if not move the idx--
+        for(int i=n-2;i>=0;i--){
 
-            // can we reach the current final position?
             if(nums[i] + i >= finalPosition){
                 finalPosition = i;
             }
-        }
-
+        } 
+        
         return finalPosition ==0;
     }
 }
